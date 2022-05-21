5680bcac1ffabe4105163b483976da72f278f8cb
Amandine Arias <amandine.arias@gmail.com>
2022-05-21 12:14:39 +0200

    feat (TaskList) : conditional render for no task

--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { createPortal } from 'react-dom';
 import { TaskListContext } from '../../context/TaskListContext';
 
 import icon_check from '../../assets/icon_check.svg';
@@ -9,11 +10,29 @@ import './TaskList.css';
 export default function TaskList() {
   const { tasks } = useContext(TaskListContext);
 
+  function NoTask() {
+    return createPortal(
+      <div id="no_task">
+        <span>
+          No tasks
+          <img src={icon_check} alt="icon check" id="icon_check" />
+        </span>
+      </div>,
+      document.getElementById('actions')
+    );
+  }
+
   return (
-    <ul id="task_list">
-      {tasks.map((task) => {
-        return <Task key={task.id} task={task} />;
-      })}
-    </ul>
+    <>
+      {tasks.length ? (
+        <ul id="task_list">
+          {tasks.map((task) => {
+            return <Task key={task.id} task={task} />;
+          })}
+        </ul>
+      ) : (
+        <NoTask />
+      )}
+    </>
   );
 }
