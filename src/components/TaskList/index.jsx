291bb98bd3b035b3ec9cf5feb6e367da614efe0d
Amandine Arias <amandine.arias@gmail.com>
2022-05-21 19:37:21 +0200

    refactor (TaskList) : clean code

--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -1,26 +1,11 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { TaskListContext } from '../../context/TaskListContext';
 
-import icon_check from '../../assets/icon_check.svg';
 import Task from '../Task';
-
-import './TaskList.css';
 
 export default function TaskList() {
   const { tasks } = useContext(TaskListContext);
-
-  function NoTask() {
-    return createPortal(
-      <div id="no_task">
-        <span>
-          No tasks
-          <img src={icon_check} alt="icon check" id="icon_check" />
-        </span>
-      </div>,
-      document.getElementById('actions')
-    );
-  }
 
   return (
     <>
@@ -30,9 +15,7 @@ export default function TaskList() {
             return <Task key={task.id} task={task} />;
           })}
         </ul>
-      ) : (
-        <NoTask />
-      )}
+      ) : null}
     </>
   );
 }
