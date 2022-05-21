cc4c08d29a47cd99cb90531f4ee99b59315ab6aa
Amandine Arias <amandine.arias@gmail.com>
2022-05-21 12:15:26 +0200

    feat (Task) : handle action => delete task

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,14 +1,23 @@
+import { useContext } from 'react';
+import { TaskListContext } from '../../context/TaskListContext';
+
 import icon_trash from '../../assets/icon_trash.svg';
 import icon_edit from '../../assets/icon_edit.svg';
 
 import './Task.css';
 
 export default function Task({ task }) {
+  const { removeTask } = useContext(TaskListContext);
+
+  function handleDelete() {
+    removeTask(task.id);
+  }
+
   return (
     <li className="list_item">
       <span className="list_item-label">{task.label}</span>
       <div className="list_item-action">
-        <button className="btn_delete btn_task">
+        <button className="btn_delete btn_task" onClick={handleDelete}>
           <img src={icon_trash} alt="trash icon" />
         </button>
         <button className="btn_edit btn_task">
