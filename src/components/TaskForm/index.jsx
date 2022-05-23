c1aea2231dac62b5eae52457c90678f0aa63bfed
Amandine Arias <amandine.arias@gmail.com>
2022-05-23 17:08:50 +0200

    update (TaskForm) : namming

--- src/components/TaskForm/index.jsx
+++ src/components/TaskForm/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useRef, useEffect, useCallback } from 'react';
 import { TaskListContext } from '../../context/TaskListContext';
 
 import icon_check from '../../assets/icon_check.svg';
@@ -6,26 +6,32 @@ import icon_check from '../../assets/icon_check.svg';
 import './TaskForm.css';
 
 export default function TaskForm() {
-  const { addTask, editedTask, updateTask, cleared, clearList } =
+  const { addTask, isEdited, updateTask, isCleared, clearList } =
     useContext(TaskListContext);
 
   const [label, setLabel] = useState('');
+  const refLabel = useRef(label);
+
+  useEffect(() => {
+    refLabel.current = label;
+  });
 
   function handleChange(event) {
+    event.preventDefault();
     setLabel(event.target.value);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    !editedTask ? (addTask(label), setLabel('')) : updateTask(editedTask.id, label);
+    !isEdited ? (addTask(label), setLabel('')) : updateTask(isEdited.id, label);
   }
 
   // console.log(label);
 
   useEffect(() => {
-    editedTask ? setLabel(editedTask.label) : setLabel('');
-    // console.log(editedTask);
-  }, [editedTask]);
+    isEdited ? setLabel(isEdited.label) : setLabel('');
+    // console.log(isEdited);
+  }, [isEdited]);
 
   function handleClear() {
     clearList();
@@ -33,37 +39,36 @@ export default function TaskForm() {
 
   return (
     <form onSubmit={handleSubmit} action="?">
-      <label htmlFor="add_task">
-        <input
-          id="add_task"
-          type="text"
-          placeholder="New task"
-          value={label}
-          onChange={handleChange}
-          required
-        />
-        <section id="actions">
-          <button
-            id={editedTask ? 'action_update' : 'action_add'}
-            className="action"
-            type="submit"
-          >
-            {editedTask ? 'Update' : 'Add'}
+      <label htmlFor="add_task" />
+      <input
+        id="add_task"
+        type="text"
+        placeholder="New task"
+        value={label}
+        onChange={handleChange}
+        required
+      />
+      <section id="actions">
+        <button
+          id={editedTask ? 'action_update' : 'action_add'}
+          className="action"
+          type="submit"
+        >
+          {editedTask ? 'Update' : 'Add'}
+        </button>
+        {!isCleared ? (
+          <button id="action_clear" className="action" onClick={handleClear}>
+            Clear
           </button>
-          {!cleared ? (
-            <button id="action_clear" className="action" onClick={handleClear}>
-              Clear
-            </button>
-          ) : (
-            <div id="no_task">
-              <span>
-                No tasks
-                <img src={icon_check} alt="icon check" id="icon_check" />
-              </span>
-            </div>
-          )}
-        </section>
-      </label>
+        ) : (
+          <div id="no_task">
+            <span>
+              No tasks
+              <img src={icon_check} alt="icon check" id="icon_check" />
+            </span>
+          </div>
+        )}
+      </section>
     </form>
   );
 }
