f7385780d45ce7d2ae022bb421317ef4f9c73496
Amandine Arias <amandine.arias@gmail.com>
2022-05-23 17:26:18 +0200

    export default (utils) : custom focus style

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
 
+import './utils/handlers'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
---
+++ src/utils/handlers.js
@@ -0,0 +1,23 @@
+// MANAGING FOCUS STYLE ACCORDING TO NAVIGATION TYPE : //////////////////////////
+// KEYBOARD OR MOUSE
+var head = document.head || document.getElementsByTagName('head')[0];
+var accessStyles = head.appendChild(document.createElement('style'));
+
+/**
+ * eventListener mouse navigation : discard box-shadow
+ * @param   {string}  mousedown  [detects mouse navigation]
+ * @return  {string}  css        {box-shadow:none !important}
+ */
+document.addEventListener('mousedown', () => {
+  accessStyles.innerHTML = '* {box-shadow:none !important}';
+});
+
+/**
+ * eventListener keydown navigation : active box-shadow
+ * @param   {string}  keydown  [detects keydown navigation]
+ * @param   {boolean} e        [event is tab key || shift tab key ? y/n]
+ * @return  {string}  css      ["" no content to set the default one]
+ */
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Tab' || e.shiftKey) accessStyles.innerHTML = '';
+});
