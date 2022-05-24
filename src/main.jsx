d4763bbc8d702343dd74a5b9124fdc28ce71d89b
Amandine Arias <amandine.arias@gmail.com>
2022-05-24 17:55:04 +0200

    update (main) : ThemeProvider
    ISSUE : no success with using it...

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
 
-import './utils/handlers'
+// import ThemeProvider from './context/ThemeContext';
+
+import './utils/handlers';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+  // <ThemeProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  // </ThemeProvider>
+);
