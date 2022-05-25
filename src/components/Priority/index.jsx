dfff2624eff4d85b1033097799c72b59d3ea547b
Amandine Arias <amandine.arias@gmail.com>
2022-05-25 13:35:28 +0200

    accessiblity (Priority) : handle conditional scrolling

--- src/components/Priority/index.jsx
+++ src/components/Priority/index.jsx
@@ -1,8 +1,25 @@
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 import './Priority.css';
 
 export default function Priority({ priority, close }) {
+  // handle ARIA attributes
+  // prevent body from scrolling when popup is open
+  useEffect(() => {
+    const body = document.querySelector('body');
+    const popup = document.getElementById('priority');
+
+    priority && body.setAttribute('arias-hidden', 'true');
+    priority && popup.setAttribute('arias-hidden', 'false');
+    priority && (body.style.overflow = 'hidden');
+
+    !priority && body.setAttribute('arias-hidden', 'false');
+    !priority && (body.style.overflow = 'unset');
+  }, [priority]);
+
+
+  // popup DOM parent for portal assignation
   const DOMparent = document.getElementById('root');
 
   return createPortal(
@@ -16,7 +33,9 @@ export default function Priority({ priority, close }) {
               className="popup_item popup_item-hight popup_close"
               onClick={() => setTimeout(close, 750)}
             >
-              <label htmlFor="hight" className="popup_label">hight</label>
+              <label htmlFor="hight" className="popup_label">
+                hight
+              </label>
               <input type="radio" id="hight" name="priority" value="hight" />
               <span className="checkmark"></span>
             </div>
@@ -25,7 +44,9 @@ export default function Priority({ priority, close }) {
               className="popup_item popup_item-medium popup_close"
               onClick={() => setTimeout(close, 750)}
             >
-              <label htmlFor="medium" className="popup_label">medium</label>
+              <label htmlFor="medium" className="popup_label">
+                medium
+              </label>
               <input type="radio" id="medium" name="priority" value="medium" />
               <span className="checkmark"></span>
             </div>
@@ -34,7 +55,9 @@ export default function Priority({ priority, close }) {
               className="popup_item popup_item-low popup_close"
               onClick={() => setTimeout(close, 750)}
             >
-              <label htmlFor="low" className="popup_label">low</label>
+              <label htmlFor="low" className="popup_label">
+                low
+              </label>
               <input type="radio" id="low" name="priority" value="low" />
               <span className="checkmark"></span>
             </div>
