b2dd2d62dfa690ab6b3e257f1489cc053dca05ff
Amandine Arias <amandine.arias@gmail.com>
2022-05-25 13:18:23 +0200

    feat (hooks) : usePopup

---
+++ src/hooks/usePopup.js
@@ -0,0 +1,34 @@
+// CUSTOM POPUP HOOK
+import { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+export default function usePopup() {
+  // popup's state
+  const [isOpen, setIsOpen] = useState(false);
+
+  // close popup when open
+  function toggle() {
+    setIsOpen(!isOpen)
+    // setIsOpen(setTimeout(!isOpen, 3000));
+    // setTimeout(setIsOpen(!isOpen), 3000);
+  }
+
+  // close popup by pressing escape key when keyboard navigation
+  function escapeToClose(e) {
+    if (e.key === 'Escape') {
+      setIsOpen(false);
+    }
+  }
+
+  // remove "escapeToClose" event from DOM once occurred
+  function keyboardEscape() {
+    window.addEventListener('keydown', escapeToClose);
+    return () => window.removeEventListener('keydown', escapeToClose);
+  }
+
+  return {
+    isOpen,
+    toggle,
+    keyboardEscape,
+  };
+}
