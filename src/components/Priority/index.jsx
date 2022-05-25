c469fda74d0ef3d1f8b2f99ea13c386cf63658b8
Amandine Arias <amandine.arias@gmail.com>
2022-05-25 13:20:59 +0200

    feat (Priority) : popup component => conditional display

--- src/components/Priority/index.jsx
+++ src/components/Priority/index.jsx
@@ -1,25 +1,47 @@
+import { createPortal } from 'react-dom';
+
 import './Priority.css';
 
-export default function Priority() {
-  return (
-    <fieldset>
-      <legend>PRIORITY</legend>
+export default function Priority({ priority, close }) {
+  const DOMparent = document.getElementById('root');
 
-      <div>
-        <input type="radio" id="hight" name="priority" value="hight" checked />
-        <label for="hight">hight</label>
-      </div>
+  return createPortal(
+    <>
+      {priority ? (
+        <main autoFocus className="popup" id="priority" role="main">
+          <fieldset className="popup_content">
+            <legend className="popup_title">PRIORITY</legend>
 
-      <div>
-        <input type="radio" id="medium" name="priority" value="medium" />
-        <label for="medium">medium</label>
-      </div>
+            <div
+              className="popup_item popup_item-hight popup_close"
+              onClick={() => setTimeout(close, 750)}
+            >
+              <label htmlFor="hight" className="popup_label">hight</label>
+              <input type="radio" id="hight" name="priority" value="hight" />
+              <span className="checkmark"></span>
+            </div>
 
-      <div>
-        <input type="radio" id="low" name="priority" value="low" />
-        <label for="low">low</label>
-      </div>
-      
-    </fieldset>
+            <div
+              className="popup_item popup_item-medium popup_close"
+              onClick={() => setTimeout(close, 750)}
+            >
+              <label htmlFor="medium" className="popup_label">medium</label>
+              <input type="radio" id="medium" name="priority" value="medium" />
+              <span className="checkmark"></span>
+            </div>
+
+            <div
+              className="popup_item popup_item-low popup_close"
+              onClick={() => setTimeout(close, 750)}
+            >
+              <label htmlFor="low" className="popup_label">low</label>
+              <input type="radio" id="low" name="priority" value="low" />
+              <span className="checkmark"></span>
+            </div>
+          </fieldset>
+        </main>
+      ) : null}
+    </>,
+    DOMparent
   );
 }
