84f0b13915cf09468a308852aa80247e723265f7
Amandine Arias <amandine.arias@gmail.com>
2022-05-25 19:32:26 +0200

    feat (Priority) : dynamic value update
    + refactor props name

--- src/components/Priority/index.jsx
+++ src/components/Priority/index.jsx
@@ -1,36 +1,47 @@
-import { useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
+
+import { TaskListContext } from '../../context/TaskListContext';
 
 import './Priority.css';
 
-export default function Priority({ priority, close }) {
+export default function Priority({ isOpen, close }) {
   // handle ARIA attributes
   // prevent body from scrolling when popup is open
   useEffect(() => {
     const body = document.querySelector('body');
     const popup = document.getElementById('priority');
+    // const inputsRadio = document.querySelectorAll('[type="radio"]')
 
-    priority && body.setAttribute('arias-hidden', 'true');
-    priority && popup.setAttribute('arias-hidden', 'false');
-    priority && (body.style.overflow = 'hidden');
+    isOpen && body.setAttribute('arias-hidden', 'true');
+    isOpen && popup.setAttribute('arias-hidden', 'false');
+    isOpen && (body.style.overflow = 'hidden');
+    // console.log([...inputsRadio].map((input) => input.value))
 
-    !priority && body.setAttribute('arias-hidden', 'false');
-    !priority && (body.style.overflow = 'unset');
-  }, [priority]);
+    !isOpen && body.setAttribute('arias-hidden', 'false');
+    !isOpen && (body.style.overflow = 'unset');
+  }, [isOpen]);
 
   // popup DOM parent for portal assignation
   const DOMparent = document.getElementById('root');
 
+  const { isEdited, updateTask } = useContext(TaskListContext);
+
+  function handlePriority(level) {
+    isEdited && updateTask(isEdited.id, isEdited.label, level);
+    setTimeout(close, 500);
+  }
+
   return createPortal(
     <>
-      {priority ? (
+      {isOpen ? (
         <main autoFocus className="popup" id="priority" role="main">
           <fieldset className="popup_content">
             <legend className="popup_title">PRIORITY</legend>
 
             <div
               className="popup_item popup_item-hight popup_close"
-              onClick={() => setTimeout(close, 500)}
+              onClick={() => handlePriority('hight')}
             >
               <label htmlFor="hight" className="popup_label">
                 hight
@@ -41,7 +52,7 @@ export default function Priority({ priority, close }) {
 
             <div
               className="popup_item popup_item-medium popup_close"
-              onClick={() => setTimeout(close, 500)}
+              onClick={() => handlePriority('medium')}
             >
               <label htmlFor="medium" className="popup_label">
                 medium
@@ -52,7 +63,7 @@ export default function Priority({ priority, close }) {
 
             <div
               className="popup_item popup_item-low popup_close"
-              onClick={() => setTimeout(close, 500)}
+              onClick={() => handlePriority('low')}
             >
               <label htmlFor="low" className="popup_label">
                 low
