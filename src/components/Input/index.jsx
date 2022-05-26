4cae60787a494a49ec48d5eeda32021891edbba4
Amandine Arias <amandine.arias@gmail.com>
2022-05-26 18:21:34 +0200

    feat (components) : Input

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,18 @@
+export default function Input({ level, handlePriority }) {
+  return (
+    <label
+      htmlFor={level}
+      className={`popup_label popup_label-${level} popup_close`}
+    >
+      {level}
+      <input
+        type="radio"
+        id={level}
+        name="priority"
+        value={level}
+        onClick={handlePriority}
+      />
+      <span className="checkmark"></span>
+    </label>
+  );
+}
